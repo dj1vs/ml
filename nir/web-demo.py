9493d92f4fd22a3eb9455d2588209fc13b28f3ee
dj1vs <icemcpe099@gmail.com>
2024-05-28 13:55:23 +0300

    make nir web-demo more descriptive

--- nir/web-demo.py
+++ nir/web-demo.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 @st.cache_data
 def load_data():
@@ -15,7 +16,7 @@ def preprocess_data(df):
     df = df.drop_duplicates()
     df['country'] = df['country'].fillna("Global")
     df = df.dropna()
-    df = df.sample(n=100, random_state=73)
+    df = df.sample(n=500, random_state=73)
     df = df.drop(columns=['daily_rank', 'daily_movement', 'weekly_movement', 'snapshot_date', 'spotify_id'])
     df = df.drop(columns=['album_name', 'artists', 'name', 'album_release_date'])
     country_label_encoder = LabelEncoder()
@@ -50,8 +51,50 @@ def draw_graph(x_train, x_test, y_train, y_test, mss, msl):
 
 st.subheader('Метод случайного леса')
 
+if st.checkbox('Описание метода'):
+    st.markdown("""
+Алгоритм случайного леса сочетает в себе две основные идеи: метод бэггинга и метод случайных подпространств.
+
+Случайный лес можно рассматривать как алгоритм бэггинга над решающими деревьями.
+
+Но при этом каждое решающее дерево строится на случайно выбранном подмножестве признаков.
+Эта особенность называется "feature bagging" и основана на методе случайных подпространств.
+
+Метод случайных подпространств позволяет снизить коррелированность между деревьями и избежать переобучения. 
+Базовые алгоритмы обучаются на случайно выбранных подмножествах признаков.
+Ансамбль моделей, использующих метод случайного подпространства, можно построить, используя следующий алгоритм:
+
+Пусть количество объектов для обучения равно $N$, а количество признаков $D$.
+Выбирается число отдельных моделей в ансамбле $L$.
+Для каждой отдельной модели $l = 1..L$ выбирается число признаков $dl (dl < D)$. Как правило, для всех моделей используется только одно значение
+$dl$.
+
+Для каждой отдельной модели $l$ создается обучающая выборка путем отбора $dl$ признаков из $D$.
+Производится обучение моделей $l = 1..L$, каждая модель обучается на отдельном подмножестве из $dl$ признаков.
+Чтобы применить модель ансамбля к тестовой выборке, объединяются результаты отдельных моделей или мажоритарным голосованием или более сложными способами.
+                
+---
+В конкретно моей НИР модель случайного леса показала наилучшие результаты, поэтому я выбрал её для визуализации.
+""")
+
+if st.checkbox('Описание гиперпараметров и метрик'):
+    st.markdown("""
+            Для оценки качеста модели используется средняя абсолютная погрешность (**mean_absolute_error**)
+
+            Можно изменить гиперпараметры модели:
+            - **min_samples_leaf**: определяет количество сэмплов, которое должно быть у ноды-листка
+            - **min_samples_split**: определяет количество сэмплов, которое нужно, чтобы разделить внутренний узел.
+            """)
+    
 data = load_data()
 data = preprocess_data(data)
+
+if st.checkbox('Корреляционная матрица'):
+    fig, ax = plt.subplots(figsize=(10,5))
+    sns.heatmap(data.corr(), annot=True, fmt='.2f')
+    st.pyplot(fig)
+
+
 x_train, x_test, y_train, y_test = split_data(data)
 
 mss_slider = st.sidebar.slider('mean samples split: ', min_value = 1, max_value = 10, value=2, step=1)
